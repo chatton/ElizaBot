ff29bcece336cf3fd57ef51fae61f1d9edd70599
chatton <cianhatton@gmail.com>
2017-11-12 23:40:11 +0000

    added Eliza functionality

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 // code adapated from this tutorial https://www.youtube.com/watch?v=024upsEuHaU
 const Discord = require("discord.js");
+const requestify = require("requestify");
 const settings = require("./botSettings.json");
+const unirest = require('unirest');
 
 const bot = new Discord.Client();
 
-bot.on("ready", () => {
-    console.log("ElizaBot Logged in!");
-});
-
 bot.on("message", message => {
     if (message.author.bot) return; // don't reply to other bots (or messages sent by this one!)
-    let text = message.content;
-    let reply = "You said: " + text;
-    message.reply(reply);
+    let text = message.content;        
+    unirest.post('http://localhost:8080/ask')
+        .send({ "question": text })
+        .end(response => {
+        message.reply(response.body);
+    });
 });
 
 bot.login(settings.token);
