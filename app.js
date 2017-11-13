e9496e7564ac8b71726c6b07522d793c84877e8c
chatton <cianhatton@gmail.com>
2017-11-13 00:10:23 +0000

    added more comments, removed unneeded require statements

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 // code adapated from this tutorial https://www.youtube.com/watch?v=024upsEuHaU
-const Discord = require("discord.js");
-const requestify = require("requestify");
-const settings = require("./botSettings.json");
+const discord = require("discord.js");
+const settings = require("./botSettings.json"); // holds onto the token required to login.
 const unirest = require('unirest');
 
-const bot = new Discord.Client();
+const eliza = new discord.Client();
 
-bot.on("message", message => {
-    if (message.author.bot) return; // don't reply to other bots (or messages sent by this one!)
-    let text = message.content;        
-    unirest.post('http://localhost:8080/ask')
-        .send({ "question": text })
+eliza.on("message", message => {
+    if (message.author.bot) {
+        return; // don't reply to other bots (or messages sent by this one!)
+    } 
+    let text = message.content; // get the actual text from the message sent
+    unirest.post('http://localhost:8080/ask') // only works on localhost currently.
+        .send({question : text}) // send text as a post parameter
         .end(response => {
-        message.reply(response.body);
-    });
+             message.reply(response.body); 
+        }); // reply to the message with eliza's answer.
 });
 
-bot.login(settings.token);
+eliza.login(settings.token);
